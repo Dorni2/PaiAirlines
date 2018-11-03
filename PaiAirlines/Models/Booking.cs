5a37be8f8ae9b9ba14471db443fc5fe81e508ef4
Dor <dorni2@gmail.com>
2018-11-03 11:25:30 +0200

    Added display names for models

--- PaiAirlines/Models/Booking.cs
+++ PaiAirlines/Models/Booking.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace PaiAirlines.Models
 {
@@ -11,12 +12,20 @@ namespace PaiAirlines.Models
 
         public int ID { get; set; }
 
+        [Required]
+        [Display(Name = "User ID")]
         public int Userid { get; set; }
 
+        [Required]
+        [Display(Name = "Fligt ID")]
         public int FlightID { get; set; }
 
+        [Required]
+        [Display(Name = "Tickets Purchased")]
         public int SeatsAmount { get; set; }
 
+        [Required]
+        [Display(Name = "Total Price")]
         public int TotalPrice { get; set; } 
 
         #endregion
